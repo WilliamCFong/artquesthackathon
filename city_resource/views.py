bb9a9dd0bd7907b1e9f334acbf407a6c0b8576cb
William Fong <willfong@mit.edu>
2021-06-19 11:11:39 -0400

    beginning front end rendering

--- city_resource/views.py
+++ city_resource/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+def city_datasource(request, slug):
+    try:
+        data_source = CityDataSource.objects.get(slug=slug)
+    except CityDataSource.DoesNotExist:
+        return HttpResponseNotFound("Doesn't exist")
+
+    context = {
+        "data_source": data_source
+    }
+
+    return render("city_resource/datasource.html", context=context)
